e1745442778c47b8dfd8f4928ce9302340679855
trinimeza <a01634249@itesm.mx>
2021-08-20 04:04:44 +0000

    Actividad entregada

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    calif1= float(input("Calificación de la materia: "))
+    calif2= float(input("Calificación de la materia: "))
+    calif3= float(input("Calificación de la materia: "))
+    calif4= float(input("Calificación de la materia: "))
+    promedio=((calif1+calif2+calif3+calif4)/4)
+    print("El promedio es:", promedio)
+
 
 if __name__ == '__main__':
     main()
